5d3faf7218ad66b93f6168e51ba2911b7ca97b6e
Ubuntu <ubuntu@ip-172-31-15-118.ap-south-1.compute.internal>
2026-04-21 21:33:52 +0530

    modified jenkinsfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Good Evening Its 8:30Pm radha radha');
+  res.send('time is 21:33');
 });
 
 app.get('/health', (req, res) => {
