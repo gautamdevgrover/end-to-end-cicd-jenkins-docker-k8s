fe7c450e3be054687006a617acaf7a5a59fe035a
gautamdevgrover <gautamdevgrover@gmail.com>
2026-04-22 12:35:40 +0530

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('time is' new Date().toString());
+  res.send(new Date().toString());
 });
 
 app.get('/health', (req, res) => {
